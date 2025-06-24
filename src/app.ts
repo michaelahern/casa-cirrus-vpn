907f4fe70f6da80717baa9713b61f98288ab5001
Michael Ahern <michael.ahern@gmail.com>
2025-06-24 02:24:24 +0000

    Init CDK Lambda

--- src/app.ts
+++ src/app.ts
@@ -3,8 +3,6 @@ import { RefreshCustomerGateways } from './refresh-customer-gateways.js';
 
 const app = new cdk.App();
 
-if (process.env.CDK_DEFAULT_REGION === 'us-east-2') {
-    new RefreshCustomerGateways(app, 'RefreshCustomerGateways', {
-        env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION }
-    });
-}
+new RefreshCustomerGateways(app, 'RefreshCustomerGateways', {
+    env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION }
+});
--- src/refresh-customer-gateways.ts
+++ src/refresh-customer-gateways.ts
@@ -1,10 +1,22 @@
 import * as cdk from 'aws-cdk-lib';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
 import { Construct } from 'constructs';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
 
 export class RefreshCustomerGateways extends cdk.Stack {
     constructor(scope: Construct, id: string, props?: cdk.StackProps) {
         super(scope, id, props);
 
-        console.log('Hello World');
+        new lambda.Function(this, 'RefreshCustomerGateways', {
+            runtime: lambda.Runtime.NODEJS_22_X,
+            code: lambda.Code.fromAsset(path.join(path.dirname(fileURLToPath(import.meta.url)), '../dist/lambdas/refresh-customer-gateways')),
+            handler: 'module.handler',
+            timeout: cdk.Duration.minutes(1),
+            functionName: 'RefreshCustomerGateways',
+            memorySize: 192,
+            tracing: lambda.Tracing.ACTIVE,
+            architecture: lambda.Architecture.ARM_64
+        });
     }
 }
